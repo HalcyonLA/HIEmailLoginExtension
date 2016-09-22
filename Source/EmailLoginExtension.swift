09fcca4ce17b193f83c614cc5841513a7d77fb68
Vlad Getman <ibrightsider@gmail.com>
2016-09-22 12:16:59 +0300

    Migration to Swift 3

--- Source/EmailLoginExtension.swift
+++ Source/EmailLoginExtension.swift
@@ -11,35 +11,36 @@ import HILoginManager
 import SwiftyUserDefaults
 import KeychainSwift
 
-public class EmailLoginExtension: NSObject {
+open class EmailLoginExtension: NSObject {
     
 }
 
 extension EmailLoginExtension: LoginProtocol {
-    public func request(completion: LoginCompletion) {
+    
+    public func request(_ completion: @escaping LoginCompletion) {
         //It's not necessary, just use a login/sign up screens
     }
     
     public func authType() -> AuthType {
-        return .Email
+        return .email
     }
     
     public func hasAuthData() -> Bool {
         return (Defaults["Email"].stringValue.characters.count > 0 && KeychainSwift().get("Password") != nil)
     }
     
-    public func authData() -> [String : AnyObject] {
+    public func authData() -> [String : Any] {
         let email = Defaults["Email"].stringValue
         let password = KeychainSwift().get("Password")
         return ["email" : email, "password": password!]
     }
     
-    public func authDataFromResponse(response: AnyObject) -> [String : AnyObject] {
+    public func authDataFromResponse(_ response: Any) -> [String : Any] {
         return response as! [String : AnyObject]
     }
     
-    public func save(response: AnyObject) {
-        let parameters = response as! [String : AnyObject]
+    public func save(_ response: Any) {
+        let parameters = response as! [String : Any]
         Defaults["Email"] = parameters["email"]
         KeychainSwift().set(parameters["password"] as! String, forKey: "Password")
     }
