be05c6ab24e3e240b67fc869750e0f65953b4dba
Vlad Getman <ibrightsider@gmail.com>
2016-11-16 15:20:55 +0200

    upgrade to new version of login manager

--- HIEmailLoginExtension.podspec
+++ HIEmailLoginExtension.podspec
@@ -6,14 +6,14 @@ s.name = "HIEmailLoginExtension"
 s.module_name = "HIEmailLoginExtension"
 s.summary = "Halcyon Innovation login component."
 s.requires_arc = true
-s.version = "1.1.0"
+s.version = "2.0.0"
 s.author = { "Vlad Getman" => "ibrightsider@gmail.com" }
 
 s.license = { :type => "MIT", :file => "LICENSE" }
 s.homepage = "https://github.com/HalcyonLA/HIEmailLoginExtension"
 s.source = { :git => "https://github.com/HalcyonLA/HIEmailLoginExtension.git", :tag => "#{s.version}"}
 
-s.dependency 'HILoginManager', '~> 1.1.0'
+s.dependency 'HILoginManager', '~> 2.0.0'
 
 s.source_files = "Source/**/*.{swift}"
 
--- Source/EmailLoginExtension.swift
+++ Source/EmailLoginExtension.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 import HILoginManager
-import SwiftyUserDefaults
-import KeychainSwift
 
 open class EmailLoginExtension: NSObject {
     
@@ -25,27 +23,11 @@ extension EmailLoginExtension: LoginProtocol {
         return .email
     }
     
-    public func hasAuthData() -> Bool {
-        return (Defaults["Email"].stringValue.characters.count > 0 && KeychainSwift().get("Password") != nil)
-    }
-    
-    public func authData() -> [String : Any] {
-        let email = Defaults["Email"].stringValue
-        let password = KeychainSwift().get("Password")
-        return ["email" : email, "password": password!]
-    }
-    
     public func authDataFromResponse(_ response: Any) -> [String : Any] {
         return response as! [String : AnyObject]
     }
     
-    public func save(_ response: Any) {
-        let parameters = response as! [String : Any]
-        Defaults["Email"] = parameters["email"]
-        KeychainSwift().set(parameters["password"] as! String, forKey: "Password")
-    }
-    
     public func reset() {
-        Defaults.remove("Email")
+        
     }
 }
